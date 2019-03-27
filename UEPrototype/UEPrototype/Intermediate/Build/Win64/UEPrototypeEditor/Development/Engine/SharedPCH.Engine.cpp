dc1034883fa6637ac3e00c585b38eea31295b8da
Minusi <minusie357@gmail.com>
2019-03-27 18:46:00 +0900

    PROJECT _ UE4 4.21 프로젝트 기초 구성
    # 세부사항
    1. 프로젝트
     - 4.21버전의 엔진으로 C++ / Basic / 콘텐츠 포함 템플릿으로 프로젝트 생성

---
+++ UEPrototype/UEPrototype/Intermediate/Build/Win64/UEPrototypeEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Utsuho/Desktop/General/Project/Prototype/UEPrototype/UEPrototype/Intermediate/Build/Win64/UEPrototypeEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ UEPrototype/UEPrototype/Source/UEPrototype/UEPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEPROTOTYPE_API AUEPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
