cad4abaa6f34209a43c3dda652c644bfd9746ae3
han ji hoon <hun2ya9@naver.com>
2019-08-03 16:08:16 +0900

    Inventory _ ContentsInventory, ContentsRoom 제작 (#12)
    * Loader _ Build.cs 파일에 AssetRegistry 모듈 등록
    
    1. Build.cs 파일에 AssetRegistry 모듈 등록
     - 비동기적으로 로드할 Asset을 검색하고 관리 하는데 효율적인 모듈
    
    2. Loader 클래스
     - 설정된 Object 타입에 해당하는 Asset들을 각각의 배열에 저장함
     - 원하는 Object 타입의 Asset배열을 반환
    
    * Inventory _ 아이템, 에셋 인벤토리 제작, 로더 제거
    
    1. 아이템 인벤토리
     - 아이템을 담고 꺼낼 수 있는 용도
     - 어떻게 활용될지는 모르겠지만 보통의 게임에 존재하는 그 인벤토리
    
    2. 에셋 인벤토리(뷰어)
     - 에셋을 항목 별로 보여주고 꺼내는 용도
     - 현재 에셋 인벤토리에서 에셋을 누르면 에셋의 AssetData를 받아오는 것이 가능함
     - 에셋 경로와 오브젝트 형과 이름을 조합하여 특정 폴더 안이나 데이터 테이블 등에서 에셋에 알맞은 이미지를 불러올 수 있도록 해야함
    
    3. 로더
     - 에셋을 불러오는 기능이 AssetRegistry에 의해서 아주 간단하게 가능함에 따라 만들어둔 Loader 는 제거하였고, 함수 라이브러리에 임시로 옮겨둠
     -  추후에 로드, 세이브 등을 담당할 모듈에 옮겨질 예정
    
    * ContentsRoom _ 컨텐츠 스폰, 배치 구현
    
    1. 구현 사항
     - 원점을 기준으로 일정 거리 떨어진 4방향에 오브젝트 유형별로 StaticMeshActor를 스폰하여 배치
     - 원점에서 부터의 거리, 한 행에 생성할 갯수, 좌우 간격, 상하 간격 등을 변수로 설정 간으
     - 너무 큰 Mesh의 경우 Component Bound의 Sphere Raidus값을 일정 크기 이상이면 그 값으로 Scale을 나눔
       간단하게 설명하자면 Mesh가 일정한 크기로 존재하도록 조절
    
    2. 미흡점
     - 원래 맵과 컨텐츠 룸이 있는 맵을 오가는 기능 미구현
     - 아직 선반이나 벽 등은 배치하지 않았음
     - 컨텐츠를 클릭하는 기능은 Mouse Input 쪽과 연동이 필요함
    
    * Fix _ 폴더 위치 변경에 따른 오류 수정
    
    1. 오류 수정
     - 폴더 위치가 변경되어 Gizmo에 사용하던 Mesh의 material 위치를 찾지 못해서 뜨는 오류 수정함
    
    * Inventory _ 불필요한 파일 제거 및 주석 처리, ContentsRoom C++포팅
    
    1. 수정 사항
     - Item 인벤토리와 Item는 사용하지 않을 것으로 예상하여 제거
     - ContentsRoom의 변수와 대부분 함수를 C++로 옮김
    
    * Merge _ 충돌 파일 수정
    
    1. 충돌 파일 수정
     - MinusiFrameworkLibrary를 동시에 수정해서 저장소 것을 가져옴
     - 합병 이후에 내 것을 가져와서 추가할 예정

---
+++ UEPrototype/Source/UEPrototype/Private/Inventory/ContentsInventory.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Inventory/ContentsInventory.h"
+
+// Sets default values
+AContentsInventory::AContentsInventory()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AContentsInventory::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AContentsInventory::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AContentsInventory::SetObjectType(TSubclassOf<UObject> InObjectType)
+{
+	ObjectType = InObjectType;
+}
+
+bool AContentsInventory::GetAssetInfoAtIndex(int32 Index, struct FAssetData& OutAssetData)
+{
+	if (Slots.IsValidIndex(Index))
+	{
+		OutAssetData = Slots[Index];
+		return true;
+	}
+	return false;
+}
+
+bool AContentsInventory::InsertAsset(struct FAssetData NewAssetData)
+{
+	if (NewAssetData != nullptr)
+	{
+		Slots.Add(NewAssetData);
+		return true;
+	}
+	return false;
+}
+
+void AContentsInventory::SetSlots(TArray<struct FAssetData> NewSlots)
+{
+	Slots = NewSlots;
+}
--- UEPrototype/Source/UEPrototype/Public/Utility/MinusiFrameworkLibrary.h
+++ UEPrototype/Source/UEPrototype/Public/Utility/MinusiFrameworkLibrary.h
@@ -27,19 +27,19 @@ enum class EBoolean : uint8
 /*
  *	게임프레임워크에서 제공하지 않는 기능들을 확장하여 유틸성을 제공하는 라이브러리 클래스입니다.
  */
-UCLASS(ClassGroup=Minusi)
+UCLASS(ClassGroup = Minusi)
 class UEPROTOTYPE_API UMinusiFrameworkLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
-public:
+
+	public:
 
 	// 대상 액터와 UClass를 비교한 결과를 반환합니다.
 	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofActor", Keywords = "==Actor"))
 	static bool IsTypeofActor(AActor const * Actor, AActor const * Compare);
 
 	// 대상 액터와 UClass를 비교한 결과를 Exec 출력으로 반환합니다.
-	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofActor", Keywords = "==Actor",ExpandEnumAsExecs = Execs))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofActor", Keywords = "==Actor", ExpandEnumAsExecs = Execs))
 	static void IsTypeofActor_Exec(AActor const * Actor, AActor const * Compare, EBoolean& Execs);
 
 	// 대상 언리얼 오브젝트와 UClass를 비교한 결과를 반환합니다.
@@ -47,7 +47,7 @@ public:
 	static bool IsTypeofObject(UObject const * Object, UObject const * Compare);
 
 	// 대상 언리얼 오브젝트와 UClass를 비교한 결과를 Exec 출력으로 반환합니다.
-	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofObject", Keywords = "==Object",ExpandEnumAsExecs = Execs))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofObject", Keywords = "==Object", ExpandEnumAsExecs = Execs))
 	static void IsTypeofObject_Exec(UObject const * Object, UObject const * Compare, EBoolean& Execs);
 
 	// 대상 언리얼 오브젝트와 UClass를 비교한 결과를 반환합니다.
@@ -58,32 +58,32 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (DisplayName = "IsTypeofClass", Keywords = "==Class", ExpandEnumAsExecs = Execs))
 	static void IsTypeofClass_Exec(UObject const * Object, TSubclassOf<UObject> const Compare, EBoolean& Execs);
 
-	
+
 
 
 
 	// Print String의 간소화된 로그 버전입니다. 2초간 지속됩니다.
-	UFUNCTION(BlueprintCallable, Category="Minusi", meta = (Keywords = "SLog"))
-	static void ScreenShortLog(FString const Message, FColor const Color = FColor(243,156,18));
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (Keywords = "SLog"))
+	static void ScreenShortLog(FString const Message, FColor const Color = FColor(243, 156, 18));
 
 	// Print String의 간소화된 로그 버전입니다. 5초간 지속됩니다.
-	UFUNCTION(BlueprintCallable, Category="Minusi", meta = (Keywords = "Log"))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (Keywords = "Log"))
 	static void ScreenLog(FString const Message, FColor const Color = FColor(243, 156, 18));
 
 	// Print String의 간소화된 로그 버전입니다. 10초간 지속됩니다.
-	UFUNCTION(BlueprintCallable, Category="Minusi", meta = (Keywords = "LLog"))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (Keywords = "LLog"))
 	static void ScreenLongLog(FString const Message, FColor const Color = FColor(243, 156, 18));
 
-	
+
 
 
 
 	// 특정 타입의 첫 번째 액터를 반환합니다. 이후 형변환을 수행하여 특정 타입으로 변환할 수 있습니다.
-	UFUNCTION(BlueprintCallable, Category="Minusi", meta=(WorldContext="WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (WorldContext = "WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
 	static AActor* GetSpecificFirstActorWithTag(const UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
 
 	// 특정 타입의 모든 액터를 반환합니다. 이후 형변환을 수행하여 특정 타입으로 변환할 수 있습니다.
-	UFUNCTION(BlueprintCallable, Category="Minusi", meta=(WorldContext="WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
+	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (WorldContext = "WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
 	static TArray<AActor*> GetSpecificAllActorWithTag(const UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
 
 
@@ -95,7 +95,7 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "Minusi|Math")
 	static void Snap(float Delta, float SnapInterval, bool& bCanSnap, float& SnappedDelta);
-	
+
 	UFUNCTION(BlueprintPure, Category = "Minusi|Trace")
 	static FTransform GetTransformToTraceHitResult(FHitResult HitResult, bool IsHit, FVector ActorLocation);
 
