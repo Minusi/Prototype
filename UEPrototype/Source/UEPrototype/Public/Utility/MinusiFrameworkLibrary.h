cd8d055255255dbffd58169d1a27c1fa2e443696
han ji hoon <hun2ya9@naver.com>
2019-08-08 01:25:31 +0900

    Inventory _ Thumbnail Generator 제작, Disposer 연동 (#16)
    * Inventory _ ThumbnailGenerator 구현, 적용, Disposer 연동
    
    1. Thumbnail Generator
     - 에디터 단계에서 개발자가 조작
     - 루트가 아닌 특정 폴더의 에셋만 가져오도록 임시로 설정
     - Inventory/Thumbnail/ 의 경로에 Mesh, Material 의 폴더로 썸네일 텍스쳐가 저장
    
    2. Disposer 연동
     - 컨텐츠 인벤토리에서 매시 컨텐츠를 선택 시 Disposer 에서 정보를 받도록 함
     - 생성/삭제 또는 Material, Texutre는 이후에 Input 쪽에서 제어
    
    * Merge _ 충돌 파일 수정

--- UEPrototype/Source/UEPrototype/Public/Utility/MinusiFrameworkLibrary.h
+++ UEPrototype/Source/UEPrototype/Public/Utility/MinusiFrameworkLibrary.h
@@ -10,8 +10,8 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/Actor.h"
+#include "UObject/Package.h"
 #include "MinusiFrameworkLibrary.generated.h"
-
 
 
 UENUM(BlueprintType)
@@ -20,8 +20,6 @@ enum class EBoolean : uint8
 	T			UMETA(DisplayName = "True"),
 	F			UMETA(DisplayName = "False")
 };
-
-
 
 
 /*
@@ -80,30 +78,36 @@ class UEPROTOTYPE_API UMinusiFrameworkLibrary : public UBlueprintFunctionLibrary
 
 	// 특정 타입의 첫 번째 액터를 반환합니다. 이후 형변환을 수행하여 특정 타입으로 변환할 수 있습니다.
 	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (WorldContext = "WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
-	static AActor* GetSpecificFirstActorWithTag(const UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
+	static AActor* GetSpecificFirstActorWithTag(const class UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
 
 	// 특정 타입의 모든 액터를 반환합니다. 이후 형변환을 수행하여 특정 타입으로 변환할 수 있습니다.
 	UFUNCTION(BlueprintCallable, Category = "Minusi", meta = (WorldContext = "WorldContextObject", Keywords = "GetTag", DeterminesOutputType = "ActorToFind"))
-	static TArray<AActor*> GetSpecificAllActorWithTag(const UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
+	static TArray<AActor*> GetSpecificAllActorWithTag(const class UObject* WorldContextObject, FName Tag, TSubclassOf<AActor> ActorToFind);
 
 
+	// 외적을 사용하여 두 백터 사이의 각도를 구함
 	UFUNCTION(BlueprintPure, Category = "Minusi|Math")
 	static void GetAngleBetweenTwoVector(FVector2D A, FVector2D B, float& Angle);
 
+	// 두 엑터간의 거리를 일차원 함수의 변수로 넣어 반환
 	UFUNCTION(BlueprintPure, Category = "Minusi|Math")
 	static float DistanceRatioByOneDimensionalFunction(AActor* StartActor, AActor* EndActor, float  RatioMul, float RatioPlus, float MinSize, float MaxSize);
 
+	// 스냅 간격을 기준으로 스냅 가능 여부, 스냅 비율 반환
 	UFUNCTION(BlueprintPure, Category = "Minusi|Math")
 	static void Snap(float Delta, float SnapInterval, bool& bCanSnap, float& SnappedDelta);
 
+	// 라인 트레이스의 결과값으로 맞은 물체와 특정 위치까지의 트랜스폼(위치, 각도) 반환
 	UFUNCTION(BlueprintPure, Category = "Minusi|Trace")
 	static FTransform GetTransformToTraceHitResult(FHitResult HitResult, bool IsHit, FVector ActorLocation);
 
+	// 원점과 방향을 화면상에 각각 사영시켜 원점에서 부터의 단위 방향 벡터를 구함
 	UFUNCTION(BlueprintPure, Category = "Minusi|Math")
-	static void ProjectWorldDirectionToScreenFromOrigin(APlayerController* PC, FVector InDirection, FVector2D& ProjectedUnitDirectionToScreen);
+	static void ProjectWorldDirectionToScreenFromOrigin(class APlayerController* PC, FVector InDirection, FVector2D& ProjectedUnitDirectionToScreen);
 
-
-
+	// 오브젝트 타입으로 루트에서 부터 에셋 데이터를 조사하여 반환
+	UFUNCTION(BlueprintPure, Category = "Minusi|Loader")
+	static TArray<struct FAssetData> GetAssetDataByObjectType(TSubclassOf<UObject> ObjectType);
 
 
 	UFUNCTION(BlueprintCallable, Category = "Minusi|UObject")
@@ -112,4 +116,6 @@ class UEPROTOTYPE_API UMinusiFrameworkLibrary : public UBlueprintFunctionLibrary
 	/* 오브젝트 이름 및 유니크 ID, 오브젝트 플래그와 Outer를 출력합니다 */
 	UFUNCTION(BlueprintCallable, Category = "Minudi|UObject")
 	static void GetInfo(const UObject* Object);
+
 };
+
